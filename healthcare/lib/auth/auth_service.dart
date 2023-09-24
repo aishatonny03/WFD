05328a74444ffda4bf42fff2adaf63314f8ae943
aishatonny03 <aat597@bnxnetworks.com>
2023-09-24 09:35:14 +0600

    Wibicki Family Dental App

---
+++ healthcare/lib/auth/auth_service.dart
@@ -0,0 +1,163 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:crypto/crypto.dart';
+
+class AuthService {
+  final String baseUrl = 'http://76.150.246.167:241';
+
+  Future<void> storeToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('jwt_token', token);
+  }
+
+  Future<String?> getStoredToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('jwt_token');
+  }
+
+  Future<String?> login(String username, String password) async {
+    try {
+      final tokens = await _fetchTokensFromServer(username, password);
+      final accessToken = tokens?['access'];
+      final refreshToken = tokens?['refresh'];
+
+      if (accessToken != null && refreshToken != null) {
+        // Store the token
+        await storeToken(accessToken);
+
+        return accessToken;
+      } else {
+        throw Exception('Login failed: Invalid token data');
+      }
+    } catch (e) {
+      if (e is Exception) {
+        final errorMessage = e.toString();
+        if (errorMessage.contains("Token retrieval failed with status code: 401")) {
+          throw Exception('Login failed: Invalid username or password');
+        } else {
+          throw Exception('Login failed: $errorMessage');
+        }
+      } else {
+        throw Exception('Login failed: An unknown error occurred');
+      }
+    }
+  }
+
+  Future<String?> register(String username, String email, String password) async {
+    try {
+      // Hash the password before sending it to the server
+      final hashedPassword = _hashPassword(password);
+
+      final response = await http.post(
+        Uri.parse('$baseUrl/fastapi/userview/register/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'email': email, 'password': hashedPassword}),
+      );
+
+      print('Response status code: ${response.statusCode}');
+      print('Response headers: ${response.headers}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 201) {
+        final token = json.decode(response.body)['access'] as String?;
+        return token;
+      } else {
+        final errorMessage = json.decode(response.body)['error_message'] as String?;
+        if (errorMessage != null) {
+          print('Error message: $errorMessage');
+        }
+        print('Registration failed');
+        return null; // Return null in case of failure.
+      }
+    } catch (e) {
+      print('Registration failed with exception: $e');
+      return null;
+    }
+  }
+
+  Future<String?> refreshToken(String refreshToken) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/fastapi/userview/refreshtoken/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'refresh_token': refreshToken}),
+      );
+
+      if (response.statusCode == 200) {
+        final token = json.decode(response.body)['access'];
+        return token;
+      } else {
+        throw Exception(_parseError(response));
+      }
+    } catch (e) {
+      throw Exception('Failed to refresh token: $e');
+    }
+  }
+
+  Future<String?> fetchProtectedData(String token, String url) async {
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final userData = json.decode(response.body);
+        return json.encode(userData); // Convert the data to a JSON string
+      } else {
+        throw Exception(_parseError(response));
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch protected data: $e');
+    }
+  }
+
+  Future<Map<String, String>?> _fetchTokensFromServer(String username, String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/gettoken/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'password': password}),
+      );
+
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        final data = json.decode(response.body) as Map<String, dynamic>;
+        final accessToken = data['access'] as String?;
+        final refreshToken = data['refresh'] as String?;
+
+        if (accessToken != null && refreshToken != null) {
+          return {'access': accessToken, 'refresh': refreshToken};
+        } else {
+          throw Exception('Token retrieval failed: Invalid token data');
+        }
+      } else {
+        print(response.statusCode);
+        throw Exception('Token retrieval failed with status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Token retrieval failed: $e');
+    }
+  }
+
+  String _parseError(http.Response response) {
+    try {
+      final errorMap = json.decode(response.body);
+      final errorMessage = errorMap['detail'] ?? 'An error occurred.';
+      return errorMessage;
+    } catch (e) {
+      return 'An error occurred.';
+    }
+  }
+
+  // Hash the password using SHA256
+  String _hashPassword(String password) {
+    final bytes = utf8.encode(password);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+}
---
+++ healthcare/lib/screens/home_screen.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/material.dart';
+import 'package:healthcare/screens/appointment_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  List<String> symptoms = [
+    "From 2 to 102",
+    "Healthy Gums!",
+    "Implants and their restoration",
+    "Toothache First-aid",
+    "Smile Beautification",
+    "Crowns, Bridges and Dentures",
+  ];
+
+  List<String> imgs = [
+    "doctor1.jpg",
+    "doctor2.jpg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Welcome to Wibicky Family Dental!",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundImage: AssetImage("images/user.jpeg"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF7165D6),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 6,
+                          spreadRadius: 4,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Image.asset(
+                              'images/boy.png',
+                              width: 150,
+                              height: 100,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Text(
+                          "No Insurance",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "No Problem",
+                          style: TextStyle(
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 6,
+                          spreadRadius: 4,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF0EEFA),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Image.asset(
+                              'images/couple.png',
+                              width: 150,
+                              height: 100,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Text(
+                          "Our Doctors",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Have Experience",
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Text(
+                "Our Services",
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 170,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: symptoms.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF4F6FA),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        symptoms[index],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Text(
+                "Meet Our Doctors",
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                // Navigate to the AppointmentScreen when a doctor is tapped
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AppointmentScreen(),
+                  ),
+                );
+              },
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemCount: 2,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage("images/${imgs[index]}"),
+                        ),
+                        const Text(
+                          "Dr. Doctor Name",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        Text(
+                          "Dentist",
+                          style: TextStyle(
+                            color: Colors.black45,
+                          ),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            Text(
+                              "4.9",
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
